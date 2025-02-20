596578df64c0e5ea075fb1febde4d383eb00dcdf
Clupai8o0 <clupaio4@gmail.com>
2025-02-21 02:39:46 +1100

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,26 @@
+import Bounded from "@/components/bounded";
+import { Button } from "@/components/ui/button";
+import { ShoppingBag } from "lucide-react";
+import Link from "next/link";
+
 export default function Home() {
-	return <main></main>;
+	return (
+		<main>
+			<Bounded>
+				<header className="flex justify-between items-center py-8 w-full">
+					<Link href="/">
+						<img
+							src="/logo.png"
+							alt="Aarav Verma handwriting logo"
+							className="w-auto h-16"
+						/>
+					</Link>
+
+					<div>
+						<ShoppingBag size={36} strokeWidth={1} />
+					</div>
+				</header>
+			</Bounded>
+		</main>
+	);
 }
